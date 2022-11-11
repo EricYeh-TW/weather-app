6f81b501add3d0e778a166df60838fe4d5070eb4
Eric Yeh <spu8673@gmail.com>
2022-11-11 16:26:58 +0800

    fix: return wrong type of data from fetch edit: add some options to dropdown menu & style

--- src/js/index.js
+++ src/js/index.js
@@ -4,26 +4,24 @@ import * as bootstrap from 'bootstrap';
 const https = 'https://opendata.cwb.gov.tw/api/v1/rest/datastore';
 const authorization = 'CWB-A1EDD3E2-7930-40B2-A1C0-E780BA36DBE4';
 
-function fetchWeatherForecast() {
+async function fetchWeatherForecast() {
   let elements = ['Wx', 'PoP', 'MinT', 'MaxT'];
   const url = `${https}/F-C0032-001?Authorization=${authorization}&format=JSON&elementName=${elements.join()}`;
-  fetch(url)
-    .then((res) => res.json())
-    .then((result) => {
-      console.log(result);
-    })
-    .catch((error) => console.log(error));
+  let data = await fetchURL(url);
+  let forecastData = data.records.location;
 }
 
-function fetchWeatherObservation() {
+async function fetchWeatherObservation() {
   let elements = ['TEMP', 'Weather'];
   const url = `${https}/O-A0003-001?Authorization=${authorization}&format=JSON&elementName=${elements.join()}&parameterName=CITY`;
-  fetch(url)
-    .then((res) => res.json())
-    .then((result) => {
-      console.log(result);
-    })
-    .catch((error) => console.log(error));
+  let data = await fetchURL(url);
+  let observeData = data.records.location;
 }
 
-fetchWeatherObservation();
+const fetchURL = async (url) => {
+  let response = await fetch(url);
+  let data = await response.json();
+  return data;
+};
+
+fetchWeatherForecast();
