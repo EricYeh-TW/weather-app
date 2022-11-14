6151e9a772830118c46d40214b6eab3a2133751b
Eric Yeh <spu8673@gmail.com>
2022-11-14 16:10:56 +0800

    feat: display weather forecast by dropdown items

--- src/js/index.js
+++ src/js/index.js
@@ -3,20 +3,23 @@ import * as bootstrap from 'bootstrap';
 
 const https = 'https://opendata.cwb.gov.tw/api/v1/rest/datastore';
 const authorization = 'CWB-A1EDD3E2-7930-40B2-A1C0-E780BA36DBE4';
+let cache = '臺北市';
 
-async function fetchWeatherForecast() {
+const fetchWeatherForecast = async (city) => {
   let elements = ['Wx', 'PoP', 'MinT', 'MaxT'];
   const url = `${https}/F-C0032-001?Authorization=${authorization}&format=JSON&elementName=${elements.join()}`;
   let data = await fetchURL(url);
-  return data;
-}
+  return data.records.location.filter((data) => data.locationName === city);
+};
 
-async function fetchWeatherObservation() {
+const fetchWeatherObservation = async (city) => {
   let elements = ['TEMP', 'TIME', 'Weather'];
   const url = `${https}/O-A0003-001?Authorization=${authorization}&format=JSON&elementName=${elements.join()}&parameterName=CITY,TOWN`;
   let data = await fetchURL(url);
-  return data;
-}
+  return data.records.location
+    .filter((data) => data.parameter[0].parameterValue === city)
+    .filter((data) => data.weatherElement[0].elementValue !== '-99');
+};
 
 const fetchURL = async (url) => {
   let response = await fetch(url);
@@ -27,33 +30,51 @@ const fetchURL = async (url) => {
 function handleClick(e) {
   let el = e.target;
   if (el.classList.value.split(' ').includes('dropdown-toggle')) return;
-  display(el.dataset.name);
+  // prevent same result
+  if (cache === el.dataset.name) return;
+  cache = el.dataset.name;
+  display(cache);
 }
 
-async function display(city) {
+const display = async (city) => {
   let toggle = document.querySelector('.dropdown-toggle');
-  let title = document.querySelector('.title');
-  let [currentTown, currentStation, currentTemp, currentWeather] = ['town', 'station', 'temp', 'weather'].map(
-    (elem) => {
-      return document.querySelector(`.current__${elem}`);
-    },
-  );
-  let observeData = await fetchWeatherObservation();
-  let locationData = observeData.records.location
-    .filter((data) => data.parameter[0].parameterValue === city)
-    .filter((data) => data.weatherElement[0].elementValue !== '-99');
+  let title = document.querySelector('.title > span');
+  let current = document.querySelector('article');
+  let [firstDay, secondDay, thirdDay] = ['first', 'second', 'third'].map((elem) => {
+    return document.querySelector(`.${elem}__day`);
+  });
+  let observeData = await fetchWeatherObservation(city);
+  let forecastData = await fetchWeatherForecast(city);
+  let today = new Date();
+  let style = 'm-0 p-0 fw-bold';
 
-  console.log(locationData);
+  // title
+  [toggle, title].map((elem) => (elem.innerHTML = city));
 
-  toggle.innerHTML = city;
-  title.innerHTML = `${city}現在氣溫及天氣預報`;
-  currentTown.innerHTML = locationData[0].parameter.reduce((name, cur) => {
-    return (name += cur.parameterValue);
-  }, '');
-  currentStation.innerHTML = `站別: ${locationData[0].locationName}`;
-  currentTemp.innerHTML = `${locationData[0].weatherElement[0].elementValue} °C`;
-  currentWeather.innerHTML = locationData[0].weatherElement[1].elementValue;
-}
+  // observe
+  // prettier-ignore
+  current.innerHTML = `
+    <h2 class="${style}">${observeData[0].parameter.reduce((name, cur) => name += cur.parameterValue, "")}</h2>
+    <small class="${style}">站別: ${observeData[0].locationName}</small>
+    <p class="${style} temp fs-1 ">${observeData[0].weatherElement[0].elementValue}</p>
+    <p class="${style} fs-4 ">${observeData[0].weatherElement[1].elementValue}</p>
+  `;
+
+  // forecast
+  [firstDay, secondDay, thirdDay].map((elem, i) => {
+    let weather = forecastData[0].weatherElement;
+    let minTemp = Number(weather[2].time[i].parameter.parameterName);
+    let maxTemp = Number(weather[3].time[i].parameter.parameterName);
+
+    elem.innerHTML = `
+      <th scope="row">${today.getMonth() + 1} / ${today.getDate() + i}</th>
+      <td>${weather[0].time[i].parameter.parameterName}</td>
+      <td class="percent">${weather[1].time[i].parameter.parameterName}</td>
+      <td class="temp">${((minTemp + maxTemp) / 2).toFixed(1)}</td>
+    `;
+  });
+};
 
 const btn = document.querySelector('.btn-group');
-btn.addEventListener('click', (e) => handleClick(e));
+btn.addEventListener('click', handleClick);
+display(cache);
