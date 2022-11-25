12dcc85737b37fbac33139835825f86c22959c68
Eric Yeh <spu8673@gmail.com>
2022-11-25 22:47:24 +0800

    chore: split scss files

--- src/js/index.js
+++ src/js/index.js
@@ -3,23 +3,8 @@ import '../scss/main.scss';
 import * as bootstrap from 'bootstrap';
 import handleClick from './handle';
 import display from './display';
-import home from '../templates/home.html';
-import about from '../templates/about.html';
 
 const btn = document.querySelector('.btn-group');
-const testButton = document.querySelector('.testButton');
 
 btn.addEventListener('click', handleClick);
 display();
-
-let isClick = false;
-
-testButton.addEventListener('click', () => {
-  const test = document.querySelector('.test');
-  isClick = !isClick;
-  if (isClick) {
-    test.innerHTML = about;
-  } else {
-    test.innerHTML = home;
-  }
-});
