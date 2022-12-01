042bb2f575ad95b740d0bdea29ce981cf30a64e5
Eric Yeh <spu8673@gmail.com>
2022-12-01 18:16:40 +0800

    feat: basic spa router

--- src/js/index.js
+++ src/js/index.js
@@ -3,8 +3,53 @@ import '../scss/main.scss';
 import * as bootstrap from 'bootstrap';
 import handleClick from './handle';
 import display from './display';
+import home from '../templates/home.html';
+import menu from '../templates/menu.html';
+import _404 from '../templates/404.html';
 
-const btn = document.querySelector('.btn-group');
+const pageTitle = 'Weather App';
 
-btn.addEventListener('click', handleClick);
-display();
+const routes = {
+  404: {
+    template: _404,
+    title: `${pageTitle} | 404`,
+    description: 'Page not found',
+  },
+  '/': {
+    template: home,
+    title: `${pageTitle} | home`,
+    description: 'This is a simple weather app',
+  },
+  menu: {
+    template: menu,
+    title: `${pageTitle} | menu`,
+    description: 'Setting for city & location & background-image',
+  },
+};
+
+function locationHandler() {
+  let location = window.location.hash.replace('#', '');
+  if (location.length === 0) {
+    location = '/';
+  }
+  const route = routes[location] || routes[404];
+  const html = route.template;
+  document.querySelector('#root').innerHTML = html;
+  document.title = route.title;
+  document.querySelector('meta[name="description"]').setAttribute('content', route.description);
+}
+
+function routeHandler(e = window.event) {
+  e.preventDefault();
+  window.history.pushState({}, '', e.target.href);
+  locationHandler();
+}
+
+window.addEventListener('hashchange', locationHandler);
+locationHandler();
+
+const menuButton = document.querySelector('.menu-btn');
+menuButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  routeHandler(e);
+});
